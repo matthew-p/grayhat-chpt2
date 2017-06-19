e9eb4874f7d8d8f6e2f4512f2106b8d04bddf7c2
m <matthewpische@gmail.com>
2017-06-18 20:33:19 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace chpt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = args[0];
+            var qIndex = url.IndexOf('?');
+            
+            if (qIndex < 0) {
+                 return;
+            }
+            var parameters = url.Remove(0, qIndex + 1).Split('&');
+            foreach (var p in parameters) {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
